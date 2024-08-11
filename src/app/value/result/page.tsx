acaa1480037ed028d6ca81bf4411203b690ba422
IkkiKyomoto <kazu06851@gmail.com>
2024-08-11 12:30:27 +0900

    minor

--- src/app/value/result/page.tsx
+++ src/app/value/result/page.tsx
@@ -5,7 +5,7 @@ import Link from "next/link";
 export default function Page({searchParams}: {  searchParams: { [key: string]: string | string[] | undefined }}) {
     const criteria = searchParams["criteria"] as string[];
   return (
-    <div className="mx-5">
+    <div className="mx-5 mb-10">
         <h2 className="text-2xl font-bold my-5 text-center">結果</h2>
       <p>あなたの企業選びの軸は…</p>
       <ValueDisplay criteria={criteria} />
